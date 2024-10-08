5650f2a54d5272f79ec4b3d388fe44512b338fd9
arteme <temik91Efremov@gmail.com>
2024-10-08 18:35:37 +0300

    Initial commit

---
+++ api.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome() -> dict:
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+	uvicorn.run("api:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
